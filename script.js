b91ff87e84f16d577b527678c10537f8622b7ca1
João Vitor <joaovitor55670@gmail.com>
2022-06-05 18:18:02 -0300

    coments

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calc() {
-    let texto = document.getElementById("texto")
-    let valor = parseInt(texto.value)
-    let h3 = document.getElementsByTagName("h3")[0]
-    var result = ""
-    for (k = 0; k <= 10; k++) {
-        result += " " + valor + " x " + k + " = " + k * valor + "<br/>"
+    let texto = document.getElementById("texto") //aq peguei o elemento com id texto
+    let valor = parseInt(texto.value) //aq peguei o valor de texto
+    let h3 = document.getElementsByTagName("h3")[0] // aq peguei o h3[0]
+    var result = "" //aq uma variável vazia pra usar dps
+    for (k = 0; k <= 10; k++) { //aq laço de repetição para repetir 10 vezes
+        result += " " + valor + " x " + k + " = " + k * valor + "<br/>" //aq juntando td
     }
-    h3.innerHTML = result
+    h3.innerHTML = result //passando resultado para o h3
 }
 
