d969d8a38c8090744e5a83fa5b25c7d8d02d7b17
João Vitor <joaovitor55670@gmail.com>
2022-09-14 20:10:53 -0300

    Final-Commit

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
 let Gerar = document.querySelector('#submit')
 
+function calcular() {
 
-
-
-
-function calcular(){
-    
     let numero = document.querySelector('#numero').value
     let resultado = document.querySelector('#p2')
-    
-    if(numero.length == 0){
+
+    if (numero.length == 0) {
         alert('Digite um número válido!')
-    }else{
-        
+    } else {
+
         resultado.innerText = ""
-        
-        
-        for(i = 0; i <= 10; i++){
 
-        resultado.innerHTML += `${numero} x ${i} = ${numero * i}<br>`
 
-    
-    
+        for (i = 0; i <= 10; i++) {
+
+            resultado.innerHTML += `${numero} x ${i} = ${numero * i}<br>`
+        }
     }
-    }
-    
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 Gerar.addEventListener('click', calcular)
 
